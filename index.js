bd7def17127a88b0de978c12b0dca5017f20dea9
code commander <stefhob@outlook.com>
2024-01-02 14:46:18 -0500

    updated generatemarkup due to incorrect spelling

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 const fs = require("fs");
 const inquirer = require("inquirer");
 const path = require("path");
-const generatMarkdown = require("./utils/generateMarkdown");
+const generateMarkdown = require("./utils/generateMarkdown");
 
 // TODO: Create an array of questions for user input
 const questions = [
