630fef5bf627c523d561b140a2e7b2842adca160
code commander <stefhob@outlook.com>
2024-01-03 16:04:13 -0500

    updated  index.js, markdown, and added README

---
+++ ExampleREADME.md
@@ -0,0 +1,38 @@
+# README Generator
+by Stefany Hobson
+![Github license](https://img.shields.io/badge/license-MIT-yellowgreen.svg)
+
+## Table of Contents
+* [Description](#description)
+* [Requirements](#requirements)
+* [Usage](#usage)
+* [Contact-Me](#contact-me)
+* [Contributors](#contributors)
+* [Testing](#testing)
+* [License](#license)
+
+
+## Description
+The README_Generator App automates the process of creating a README.md file for developer projects.
+
+## Requirements
+Inquirer 8.2.4
+
+## Usage
+Answer each question and press enter. Once all questions are answered a formatted README.md file will be created.  App created using Markdown, Javascript, Node Js, Json. 
+
+## Contact-Me
+* Name - Stefany Hobson
+* Email - starbell360@hotmail.com
+* Github - [https://github.com/Winner1s](https://github.com/Winner1s/)
+
+## Contributors
+Stefany Hobson
+
+## Testing
+```
+Created example README.md file for the app
+```
+
+## License
+Licensed under the MIT license.
--- index.js
+++ index.js
@@ -10,61 +10,92 @@ const questions = [
         type: "input",
         name: "title",
         message: "Title of project",
-    }
+    },
 
     {
         type: "input",
         name: "description",
         message: "Summary of project",
-    } 
+    },
 
     {
         type: "input",
-        name: "table of contents",
-        message: "Create table of contents",
-    }
+        name: "require",
+        message: "Enter requirements",
+    },
+
+       {
+        type: "input",
+        name: "usage",
+        message: "Provide usage details" ,
+    },
+    
     {
         type: "input",
         name: "Installation",
         message: "Provide installation instructions",
-    }
+    },
+
+ 
 
     {
-        type: "input",
-        name: "Usage",
-        message: "Provide usage details" ,
-    }
-
-    {
-        type: "input",
+        type: "checkbox",
         name: "license",
-        message: "Provide license details" 
-    }
+        choices: ["MIT", "APACHE2.0", "Boost1.0", "MPL2.0", "BSD2", "none"], 
+    },
 
     {
         type: "input",
-        name: "authors",
+        name: "name",
         message: "Name of Authors and developers" 
-    }
+    },
 
     {
         type: "input",
-        name: "tests",
-        message: "include any tests for the project" 
-    }
+        name: "email",
+        message: "Enter email address using this format []()" 
+    },
 
+    {
+        type: "input",
+        name: "creator",
+        message: "Enter github.com account using this format []()" 
+    },
     {
         type: "input",
         name: "questions",
-        message: "Questions" 
-    }
+        message: "Questions",
+    },
+
+    {
+        type: "input",
+        name: "contribution",
+        message: "Who contributed to this project?",
+    },
+
+    {
+        type: "input",
+        name: "test",
+        message: "include any tests for the project" 
+    },
+
+    
 ];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    return fs.writeFileSync(path.join(process.cwd(), fileName), data);
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+// TODO: Create a function to initialize app
+function init() {
+    inquirer.prompt(questions).then((responses) => {
+      console.log("Easy README.md File");
+      writeToFile("./dist/README.md", generateMarkdown({ ...responses }));
+    });
+  }
+  
+  // Function call to initialize app
+  init();
 
-// Function call to initialize app
-init();
